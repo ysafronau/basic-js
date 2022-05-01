7972a0f2bf6aa151eef3e15d94223bb53abeacad
Yauheni Safronau <ysafronau26@gmail.com>
2022-05-01 13:21:53 +0200

    fix: carbon-dating and count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function countCats(catArray) {
+  let count = 0;
+  catArray.forEach((cat) =>
+    cat.forEach((ears) => {
+      if (ears === "^^") count++;
+    })
+  );
+  return count;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
